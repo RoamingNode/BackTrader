956c2452121ee0b07b3d2c3035d3a41589f95983
RoamingNode <74171188+RoamingNode@users.noreply.github.com>
2021-04-08 15:39:26 +0800

    Create config.py

---
+++ providers/config.py
@@ -0,0 +1,33 @@
+﻿discordBot = {
+    'ChannelId': 1234567890,
+    'Token': '1234567890qwerty-1234567890qwerty',
+    'bot': 'discordname_bot',
+    'id': 01234567890,
+    'prefix': 'Префикс бота'
+}
+
+Telegram = {
+    "TOKEN": '1234567890:ABCDEF1234567890qwerty-1234567890qwerty',
+    "chat_id": '1234567890'
+}
+
+# Quik section
+
+Host = '127.0.0.1'
+
+accountStocksRu = {'ClientCode': 'Dxxxxx',  # Код клиента (присваивается брокером)
+                   'FirmId': 'MC0063100000',  # Фирма
+                   'TradeAccountId': 'L01-00000XXX',  # Счет (Указывается Ваш счет)
+                   'LimitKind': 2,  # День лимита
+                   'CurrencyCode': 'SUR',  # Валюта
+                   'use_positions': True,  # При запуске брокера подтягиваются текущие позиции с биржи
+                   'Lots': True,  # Входящий остаток в лотах (задается брокером)
+                   }
+
+accountFuturesRu = {'ClientCode': 'Dxxxxx',  # Код клиента (присваивается брокером)
+                    'FirmId': 'SPBFUT',  # Фирма
+                    'TradeAccountId': 'SPBFUT00XXX',  # Счет (Указывается Ваш счет)
+                    'LimitKind': 0,  # День лимита
+                    'CurrencyCode': 'SUR',  # Валюта
+                    'use_positions': True,  # При запуске брокера подтягиваются текущие позиции с биржи
+                    'Lots': True,  # Входящий остаток в лотах (задается брокером)
